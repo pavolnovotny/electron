0bec006f48765b163204ad7e97fc7e10cb5455d6
Pavol Novotny <pavol.novotny@drmax.cz>
2021-12-29 13:32:24 +0100

    finished app

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-const { app, BrowserWindow, ipcMain, Notification, Menu } = require('electron')
+const { app, BrowserWindow, ipcMain, Notification, Menu, Tray } = require('electron')
 const path = require('path')
 const isDev = !app.isPackaged
 
 const dockIcon = path.join(__dirname, 'assets', 'images', 'react_app_logo.png')
+const trayIcon = path.join(__dirname, 'assets', 'images', 'react_icon.png')
 
-const createWindow = () => {
+function createSplashWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 200,
+    backgroundColor: '#6e707e',
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    }
+  })
+
+  win.loadFile('splash.html')
+  return win;
+}
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     backgroundColor: '#6e707e',
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
@@ -18,7 +38,8 @@ const createWindow = () => {
   })
 
   win.loadFile('index.html')
-  isDev && win.webContents.openDevTools()
+  isDev && win.webContents.openDevTools();
+  return win;
 }
 
 if (isDev) {
@@ -28,34 +49,48 @@ if (isDev) {
 }
 
 if (process.platform === 'darwin') {
-  app.dock.setIcon(dockIcon)
+  app.dock.setIcon(dockIcon);
 }
 
-app.whenReady().then(() => {
-  const template = require('./utils/Menu').createTemplate(app)
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
-  createWindow()
-})
+let tray = null;
+app.whenReady()
+  .then(() => {
+    const template = require('./utils/Menu').createTemplate(app);
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    tray = new Tray(trayIcon);
+    tray.setContextMenu(menu);
+
+    const splash = createSplashWindow();
+    const mainApp = createWindow();
+
+    mainApp.once('ready-to-show', () => {
+
+      setTimeout(() => {
+        splash.destroy();
+        mainApp.show();
+      }, 1500)
+    })
+  });
 
 ipcMain.on('notify', (_, message) => {
-  new Notification({
-    title: 'Notification', body: message
-  }).show()
+  new Notification({title: 'Notification', body: message}).show();
 })
 
 ipcMain.on('app-quit', () => {
   app.quit();
 })
 
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
 })
